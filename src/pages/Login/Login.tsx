f1d5f602f47eeca4e164218d7b98bbf948e6fbbf
Fadhil <fadhil@incit.org>
2025-04-12 14:29:00 +0700

    Improve the login page with better responsiveness in mobile screen size

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./components/LoginForm";
 
 export default function Login() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center p-4 justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
         <h1 className="text-2xl font-bold text-center">Login</h1>
         <LoginForm />
