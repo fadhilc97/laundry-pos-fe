4ad7356a17ea6b25c2b11fb86759449801aa2615
Fadhil <fadhil@incit.org>
2025-04-12 15:06:06 +0700

    Install and setup axios

---
+++ src/config/axios.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
