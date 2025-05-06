58e063d66b8e93c8eccd696207d66f9efd6ffefb
Fadhil <fadhil@incit.org>
2025-05-06 21:17:51 +0700

    Setup axios for API request with Authorization header

--- src/hooks/useAxiosPrivate.ts
+++ src/hooks/useAxiosPrivate.ts
@@ -1,1 +1,41 @@
-export function useAxiosPrivate() {}
+import { use, useEffect } from "react";
+import { axiosPrivate } from "@/config";
+import { AuthContext } from "@/contexts";
+import { getNewAccessTokenPromise } from "@/lib";
+
+export function useAxiosPrivate() {
+  const authContext = use(AuthContext);
+
+  useEffect(() => {
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      (config) => {
+        if (!config.headers["Authorization"]) {
+          config.headers["Authorization"] = `Bearer ${authContext.accessToken}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const prevRequest = error.config;
+        if (error.response.status === 401 && !prevRequest.sent) {
+          prevRequest.sent = true;
+          const { accessToken } = await getNewAccessTokenPromise();
+          prevRequest.headers["Authorization"] = `Bearer ${accessToken}`;
+          return axiosPrivate(prevRequest);
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestIntercept);
+      axiosPrivate.interceptors.response.eject(responseIntercept);
+    };
+  }, [authContext, getNewAccessTokenPromise]);
+
+  return axiosPrivate;
+}
