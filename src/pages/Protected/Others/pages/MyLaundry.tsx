fb65a371769074bf4a2a0185c0decd77461e7ded
Fadhil <fadhil@incit.org>
2025-05-22 21:41:47 +0700

    Integrate current user laundry details

--- src/pages/Protected/Others/pages/MyLaundry.tsx
+++ src/pages/Protected/Others/pages/MyLaundry.tsx
@@ -1,22 +1,24 @@
+import { useGetMyLaundry } from "@/hooks";
 import { MapPin } from "lucide-react";
 
 export default function MyLaundry() {
+  const getMyLaundry = useGetMyLaundry();
+  const laundry = getMyLaundry.data?.data.data;
+
   return (
     <div className="p-4 space-y-2 bg-secondary">
       <div className="border-2 mx-auto border-primary rounded-full w-fit">
         <img
-          src={"/img/default.png"}
+          src={laundry?.imageUrl || "/img/default.png"}
           alt="Laundry Profile Picture"
           width={40}
           className="rounded-full"
         />
       </div>
-      <h1 className="text-xl text-center">Your laundry name here</h1>
+      <h1 className="text-xl text-center">{laundry?.name}</h1>
       <div className="flex gap-2 items-center justify-center">
         <MapPin size={18} />
-        <p className="text-sm text-muted-foreground">
-          Your laundry location here
-        </p>
+        <p className="text-sm text-muted-foreground">{laundry?.address}</p>
       </div>
     </div>
   );
