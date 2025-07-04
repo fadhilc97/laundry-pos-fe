6a495aa07d8ea8c6652d676d8e9b36d39021b5e5
Fadhil <fadhil@incit.org>
2025-07-04 21:50:41 +0700

    Refactor the SimplePagination component

--- src/components/XPagination/SimplePagination/SimplePagination.tsx
+++ src/components/XPagination/SimplePagination/SimplePagination.tsx
@@ -6,22 +6,28 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { cn } from "@/lib";
+import { useSearchParams } from "react-router";
 
 type Props = {
-  page: number | string;
   lastPage: number;
-  onChangePage: (page: number) => void;
 };
 
-export default function SimplePagination({
-  page,
-  lastPage,
-  onChangePage,
-}: Props) {
+export default function SimplePagination({ lastPage }: Props) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = +(searchParams.get("page") || "1");
+
+  function handleChangePage(page: number) {
+    setSearchParams((prev) => {
+      const params = new URLSearchParams(prev);
+      params.set("page", page.toString());
+      return Object.fromEntries(params.entries());
+    });
+  }
+
   return (
     <Pagination>
       <PaginationContent>
-        <PaginationItem onClick={() => +page > 1 && onChangePage(+page - 1)}>
+        <PaginationItem onClick={() => page > 1 && handleChangePage(page - 1)}>
           <PaginationPrevious
             className={cn(
               +page <= 1 &&
@@ -35,11 +41,11 @@ export default function SimplePagination({
           </div>
         </PaginationItem>
         <PaginationItem
-          onClick={() => +page < lastPage && onChangePage(+page + 1)}
+          onClick={() => page < lastPage && handleChangePage(page + 1)}
         >
           <PaginationNext
             className={cn(
-              +page >= lastPage &&
+              page >= lastPage &&
                 "hover:bg-transparent hover:text-muted-foreground cursor-not-allowed text-muted-foreground"
             )}
           />
