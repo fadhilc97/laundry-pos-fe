b2595f4a22feeb1db95eb8015fec014ab350f7d9
Fadhil <fadhil@incit.org>
2025-07-04 21:51:05 +0700

    Remove unused logic in transaction list

--- src/pages/Protected/Transactions/components/TransactionList.tsx
+++ src/pages/Protected/Transactions/components/TransactionList.tsx
@@ -2,18 +2,11 @@ import { useGetTransactionList } from "@/hooks";
 import TransactionListItem from "./TransactionListItem";
 import TransactionListFilter from "./TransactionListFilter";
 import { SimplePagination } from "@/components";
-import { useSearchParams } from "react-router";
 
 export default function TransactionList() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const currentPage = searchParams.get("page") || "1";
   const getTransactionList = useGetTransactionList();
   const transactions = getTransactionList.data?.data.data;
   const metadata = getTransactionList.data?.data.metadata;
-
-  function handleChangePage(page: number) {
-    setSearchParams((prev) => ({ ...prev, page }));
-  }
 
   return (
     <div className="space-y-2">
@@ -21,11 +14,7 @@ export default function TransactionList() {
         <h1 className="font-semibold items-center text-md">Transaction List</h1>
         <TransactionListFilter />
       </div>
-      <SimplePagination
-        page={currentPage}
-        lastPage={metadata?.pagination.lastPage || 1}
-        onChangePage={handleChangePage}
-      />
+      <SimplePagination lastPage={metadata?.pagination.lastPage || 1} />
       {transactions?.map((transaction) => (
         <TransactionListItem key={transaction.id} {...transaction} />
       ))}
