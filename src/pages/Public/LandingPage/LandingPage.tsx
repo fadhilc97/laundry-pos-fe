c0e20132d4fa0e1f13622ad2b25ef538b6849446
Fadhil <fadhil@incit.org>
2025-07-05 20:54:23 +0700

    Make simple landing page

--- src/pages/Public/LandingPage/LandingPage.tsx
+++ src/pages/Public/LandingPage/LandingPage.tsx
@@ -1,5 +1,28 @@
+import { Button } from "@/components/ui/button";
 import { Link } from "react-router";
 
 export default function LandingPage() {
-  return <Link to="/login">Login here</Link>;
+  return (
+    <div className="flex flex-col px-4 justify-center items-center h-screen">
+      <h1 className="font-semibold text-2xl text-center">Laundry POS</h1>
+      <p className="text-center text-sm leading-4 px-4 mt-1">
+        Welcome to your laundry management solution.
+      </p>
+      <img
+        src="/img/laundry-illustration.png"
+        alt="Laundry Illustration, Source: https://www.vhv.rs/download/bRTTRb_washing-machine-drawing-png-transparent-png/"
+        width={72}
+        className="mt-8"
+      />
+      <Link to="/login" className="inline-block mt-8 w-full">
+        <Button
+          className="font-semibold w-full"
+          variant="default"
+          type="button"
+        >
+          Login
+        </Button>
+      </Link>
+    </div>
+  );
 }
