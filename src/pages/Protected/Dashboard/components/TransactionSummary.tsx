6fc5bce5092635cfcbc75e99732d996685c30c0b
Fadhil <fadhil@incit.org>
2025-07-12 13:17:46 +0700

    Integrate dashboard transaction summary

--- src/pages/Protected/Dashboard/components/TransactionSummary.tsx
+++ src/pages/Protected/Dashboard/components/TransactionSummary.tsx
@@ -6,12 +6,36 @@ import {
   ArrowUpDown,
 } from "lucide-react";
 import { Card } from "@/components/ui/card";
+import { useGetDashboard } from "@/hooks";
+import { TransactionStatus } from "@/lib";
 
 export function TransactionSummary() {
+  const getDashboard = useGetDashboard();
+  const dashboardData = getDashboard.data?.data.data;
+
+  function getCountAll(): number {
+    return (
+      dashboardData?.transactionCount.reduce(
+        (acc, curr) => acc + curr.count,
+        0
+      ) || 0
+    );
+  }
+
+  function getCountByStatus(status: TransactionStatus): number {
+    return (
+      dashboardData?.transactionCount.find(
+        (trxCount) => trxCount.status === status
+      )?.count || 0
+    );
+  }
+
   return (
     <Card className="rounded-lg justify-around p-4">
       <div className="flex flex-col">
-        <span className="text-4xl font-semibold text-center">20</span>{" "}
+        <span className="text-4xl font-semibold text-center">
+          {getCountAll()}
+        </span>{" "}
         <div className="flex gap-2 justify-center items-center font-semibold">
           <ArrowUpDown size={14} />
           <span className="text-center">Transactions</span>
@@ -20,14 +44,18 @@ export function TransactionSummary() {
       <div className="divide-y">
         <div className="grid grid-cols-2 divide-x gap-4">
           <div className="py-4 flex flex-col">
-            <span className="text-lg font-semibold text-center">20</span>{" "}
+            <span className="text-lg font-semibold text-center">
+              {getCountByStatus(TransactionStatus.CHECK_IN)}
+            </span>{" "}
             <div className="flex gap-2 justify-center items-center text-sm font-semibold">
               <LogIn size={14} />
               <span className="text-center">Check-in</span>
             </div>
           </div>
           <div className="py-4 flex flex-col">
-            <span className="text-lg font-semibold text-center">20</span>{" "}
+            <span className="text-lg font-semibold text-center">
+              {getCountByStatus(TransactionStatus.IN_PROCESS)}
+            </span>{" "}
             <div className="flex gap-2 justify-center items-center text-sm font-semibold">
               <LucideRefreshCw size={14} />{" "}
               <span className="text-center">In-process</span>
@@ -36,14 +64,18 @@ export function TransactionSummary() {
         </div>
         <div className="grid grid-cols-2 divide-x gap-4">
           <div className="flex py-4 flex-col">
-            <span className="text-lg font-semibold text-center">20</span>{" "}
+            <span className="text-lg font-semibold text-center">
+              {getCountByStatus(TransactionStatus.FINISHED)}
+            </span>{" "}
             <div className="flex gap-2 justify-center items-center text-sm font-semibold">
               <CheckSquare size={14} />
               <span className="text-center">Finished</span>
             </div>
           </div>
           <div className="flex py-4 flex-col">
-            <span className="text-lg font-semibold text-center">20</span>{" "}
+            <span className="text-lg font-semibold text-center">
+              {getCountByStatus(TransactionStatus.CHECK_OUT)}
+            </span>{" "}
             <div className="flex gap-2 justify-center items-center text-sm font-semibold">
               <LogOut size={14} />
               <span className="text-center">Check-out</span>
