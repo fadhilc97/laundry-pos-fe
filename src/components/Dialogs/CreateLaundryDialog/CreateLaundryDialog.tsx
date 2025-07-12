0d9e0998e726aec361040d3e86e8edcc6edcea20
Fadhil <fadhil@incit.org>
2025-07-12 16:42:09 +0700

    Show create laundry dialog only for owner user

--- src/components/Dialogs/CreateLaundryDialog/CreateLaundryDialog.tsx
+++ src/components/Dialogs/CreateLaundryDialog/CreateLaundryDialog.tsx
@@ -9,14 +9,24 @@ import {
 } from "@/components/ui/dialog";
 import { useGetMyLaundry, usePostLogout } from "@/hooks";
 import CreateLaundryForm from "./components/CreateLaundryForm";
+import { use } from "react";
+import { AuthContext } from "@/contexts";
+import { Role } from "@/lib";
 
 export default function CreateLaundryDialog() {
+  const authContext = use(AuthContext);
   const postLogout = usePostLogout();
   const getMyLaundry = useGetMyLaundry();
   const myLaundryData = getMyLaundry.data?.data.data;
 
   return (
-    <Dialog open={!myLaundryData && !getMyLaundry.isPending}>
+    <Dialog
+      open={
+        authContext.roles?.includes(Role.OWNER) &&
+        !myLaundryData &&
+        !getMyLaundry.isPending
+      }
+    >
       <DialogContent className="h-2/3 overflow-auto">
         <DialogHeader className="text-left">
           <DialogTitle>Create Laundry</DialogTitle>
