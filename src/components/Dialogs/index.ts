d16d27801e1826e84a5d437d2590fb0edd1402de
Fadhil <fadhil@incit.org>
2025-08-20 21:48:57 +0700

    Add checkbox component

--- src/components/Dialogs/index.ts
+++ src/components/Dialogs/index.ts
@@ -1,3 +1,4 @@
 export { default as CreateLaundryDialog } from "./CreateLaundryDialog/CreateLaundryDialog";
 export { default as SetLocationDialog } from "./SetLocationDialog/SetLocationDialog";
 export { default as CreatePaymentDialog } from "./CreatePaymentDialog/CreatePaymentDialog";
+export { default as CreateUserDialog } from "./CreateUserDialog/CreateUserDialog";
