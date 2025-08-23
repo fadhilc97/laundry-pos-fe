db1c695ed8437ef1a5134f98d28d876429a0c0a5
Fadhil <fadhil@incit.org>
2025-08-23 15:23:46 +0700

    Show message that users data is empty

--- src/pages/Protected/User/components/UserList.tsx
+++ src/pages/Protected/User/components/UserList.tsx
@@ -3,21 +3,25 @@ import UserListItem from "./UserListItem";
 
 export default function UserList() {
   const getUserList = useGetUserList();
-  const users = getUserList.data?.data.data;
+  const users = getUserList.data?.data.data || [];
 
   return (
     <div className="space-y-2">
       <div className="flex justify-between">
         <h1 className="font-semibold items-center text-md">User List</h1>
       </div>
-      {users?.map((user) => (
-        <UserListItem
-          key={user.id}
-          name={user.name}
-          email={user.email}
-          roles={user.roles}
-        />
-      ))}
+      {users?.length <= 0 ? (
+        <p className="text-center">Data empty</p>
+      ) : (
+        users?.map((user) => (
+          <UserListItem
+            key={user.id}
+            name={user.name}
+            email={user.email}
+            roles={user.roles}
+          />
+        ))
+      )}
     </div>
   );
 }
