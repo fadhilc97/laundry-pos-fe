26faf3e782dd21459bafc4e56e513246a6fb59b5
Fadhil <fadhil@incit.org>
2025-08-27 21:59:18 +0700

    Show more details in my laundry page

--- src/pages/Protected/Others/pages/MyLaundry.tsx
+++ src/pages/Protected/Others/pages/MyLaundry.tsx
@@ -1,24 +1,55 @@
+import { Button } from "@/components/ui/button";
 import { useGetMyLaundry } from "@/hooks";
 import { MapPin } from "lucide-react";
+import { Link } from "react-router";
 
 export default function MyLaundry() {
   const getMyLaundry = useGetMyLaundry();
   const laundry = getMyLaundry.data?.data.data;
 
   return (
-    <div className="p-4 space-y-2 bg-secondary">
-      <div className="border-2 mx-auto border-primary rounded-full w-fit">
-        <img
-          src={laundry?.imageUrl || "/img/default.png"}
-          alt="Laundry Profile Picture"
-          width={40}
-          className="rounded-full"
-        />
+    <div className="w-full">
+      <div className="p-4 space-y-2 bg-secondary">
+        <div className="border-2 mx-auto border-primary rounded-full w-fit">
+          <img
+            src={laundry?.imageUrl || "/img/default.png"}
+            alt="Laundry Profile Picture"
+            width={40}
+            className="rounded-full"
+          />
+        </div>
+        <h1 className="text-xl text-center">{laundry?.name}</h1>
+        <div className="flex gap-2 items-center justify-center">
+          <MapPin size={18} />
+          <p className="text-sm text-muted-foreground">{laundry?.address}</p>
+        </div>
       </div>
-      <h1 className="text-xl text-center">{laundry?.name}</h1>
-      <div className="flex gap-2 items-center justify-center">
-        <MapPin size={18} />
-        <p className="text-sm text-muted-foreground">{laundry?.address}</p>
+      <div className="space-y-2 p-4">
+        <div className="-space-y-1">
+          <p className="text-slate-700 text-xs font-semibold">
+            Default Currency
+          </p>
+          <p className="text-slate-500">{laundry?.currency}</p>
+        </div>
+        {laundry?.contacts.map((contact) => (
+          <div key={contact.id} className="-space-y-1">
+            <p className="text-slate-700 text-xs font-semibold capitalize">
+              {contact.name.toLocaleLowerCase()}
+            </p>
+            <p className="text-slate-500">{contact.details}</p>
+          </div>
+        ))}
+      </div>
+      <div className="px-4">
+        <Link to="/others/my-laundry/edit">
+          <Button
+            type="button"
+            className="w-full font-semibold"
+            variant="default"
+          >
+            Edit
+          </Button>
+        </Link>
       </div>
     </div>
   );
